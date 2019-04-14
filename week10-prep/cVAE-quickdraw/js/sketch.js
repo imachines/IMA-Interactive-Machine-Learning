fb913668883d98b973164427da6f1053185c7534
Aven Le Zhou <aven@artmachines.net>
2019-04-14 11:46:48 +0800

    week10 prep

---
+++ week10-prep/cVAE-quickdraw/js/sketch.js
@@ -0,0 +1,30 @@
+let cvae, index;
+
+function setup() {
+  createCanvas(640, 480);
+  cvae = ml5.CVAE('../model/quick_draw/manifest.json', function(){
+    console.log("model loaded...");
+    console.log(cvae.labels);
+  });
+  fill(255);
+  textSize(50);
+}
+
+function draw(){
+  index = int(map(mouseY, 0, height, 0, 49));
+  index = constrain(index, 0, 49);
+
+  if(cvae.ready){
+    cvae.generate(cvae.labels[index], function(err, res){
+        image(res.image, 0, 0, width, height);
+    });
+
+    text(cvae.labels[index],20,70);
+  }
+}
+
+function mouseClicked() {
+  cvae.generate(cvae.labels[index], function(err, res){
+      image(res.image, 0, 0, width, height);
+  });
+}
